171d9a1e70a1591499d2be2a931e719b316e115e
shezan2004 <shezantouhidulalam@gmail.com>
2026-05-18 20:19:38 +0600

    fix: update member and return queries to use POST method for data retrieval

--- Controllers/LibrarianOperationsController.php
+++ Controllers/LibrarianOperationsController.php
@@ -22,13 +22,13 @@ $data['unassigned_books'] = $branchId ? getBooksWithoutInventoryForBranch($conn,
 $data['pending_requests'] = $branchId ? getPendingBorrowRequestsForBranch($conn, $branchId) : array();
 $data['active_loans'] = $branchId ? getActiveLoansForBranch($conn, $branchId, '') : array();
 $data['reservations'] = $branchId ? getReservationWaitlistForBranch($conn, $branchId) : array();
-$data['members'] = $branchId && isset($_GET['member_query']) ? searchMembersByBranch($conn, $branchId, $_GET['member_query']) : array();
+$data['members'] = $branchId && isset($_POST['member_query']) ? searchMembersByBranch($conn, $branchId, $_POST['member_query']) : array();
 $data['stats'] = $branchId ? getBranchCatalogStats($conn, $branchId) : array('most_borrowed' => array(), 'never_borrowed' => array(), 'borrows_by_genre' => array());
 $data['announcements'] = $branchId ? getAnnouncementsForBranch($conn, $branchId) : array();
 $data['transfers'] = $branchId ? getInterBranchRequestsForBranch($conn, $branchId) : array();
 
-if (isset($_GET['member_id']) && $branchId) {
-    $memberId = (int)$_GET['member_id'];
+if (isset($_POST['member_id']) && $branchId) {
+    $memberId = (int)$_POST['member_id'];
     $data['member_history'] = getMemberBorrowHistory($conn, $memberId);
     $data['member_fines'] = getMemberFineHistory($conn, $memberId);
 } else {
@@ -36,21 +36,20 @@ if (isset($_GET['member_id']) && $branchId) {
     $data['member_fines'] = array();
 }
 
-if (isset($_GET['return_query']) && $branchId) {
-    $data['return_matches'] = getBorrowRecordForReturnSearch($conn, $branchId, $_GET['return_query']);
+if (isset($_POST['return_query']) && $branchId) {
+    $data['return_matches'] = getBorrowRecordForReturnSearch($conn, $branchId, $_POST['return_query']);
 } else {
     $data['return_matches'] = array();
 }
 
-if (isset($_GET['loan_filter']) && $branchId) {
-    $data['active_loans'] = getActiveLoansForBranch($conn, $branchId, $_GET['loan_filter']);
+if (isset($_POST['loan_filter']) && $branchId) {
+    $data['active_loans'] = getActiveLoansForBranch($conn, $branchId, $_POST['loan_filter']);
 }
 
 Close($conn);
 
 $_SESSION['librarian_ops'] = $data;
 
-header('Location: ../Views/Librariyan/OperationsView.php');
-exit();
+require_once '../Views/Librariyan/OperationsView.php';
 
 ?>
